4957c4b6d343e074491718607d06c52d79a2aa25
John (Jack) Brown <jack@brownjohnf.com>
2016-08-12 14:15:03 -0700

    add debug code

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+set -x
+
 if [ "$HALT" == "1" ]; then
-  echo "HALT don't do anything"
+  echo "start.sh: ENV HALT=1; don't do anything"
 	exit
 fi
 
+env
+
 if [[ -z "$AWS_CERT" || -z "$AWS_PRIVATE_KEY" || -z "$AWS_ROOT_CA" ]]; then
-	echo "Creating AWS certificates"
+	echo "start.sh: Creating AWS certificates"
 
 	curl -X POST -H "Cache-Control: no-cache" \
 		-d '{ "uuid": "'$RESIN_DEVICE_UUID'", "attributes": { "someKey": "someVal" } }' \
@@ -13,6 +18,13 @@ if [[ -z "$AWS_CERT" || -z "$AWS_PRIVATE_KEY" || -z "$AWS_ROOT_CA" ]]; then
 
 fi
 
-echo "AWS certificates exist - running app"
-node /usr/src/app/server.js
+if [[ -z ${AWS_CERT+x} || -z ${AWS_PRIVATE_KEY+x} || -z ${AWS_ROOT_CA+x} ]]; then
+	echo "fatal: start.sh: AWS_CERT, AWS_PRIVATE_KEY and/or AWS_ROOT_CA not set! exiting!"
+
+	exit 1
+else
+	echo "start.sh: AWS certificates exist - running app"
+
+	node /usr/src/app/server.js
+fi
 
